54091a0400d9237f0f3b139c3f304b61f75c1e1e
ozubergz <jmin499@gmail.com>
2021-02-03 23:32:39 -0500

    finished

--- app/src/main/java/com/example/homeworkapplication/adapter/PersonAdapter.java
+++ app/src/main/java/com/example/homeworkapplication/adapter/PersonAdapter.java
@@ -1,5 +1,62 @@
 package com.example.homeworkapplication.adapter;
 
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.homeworkapplication.databinding.ItemLayoutBinding;
+import com.example.homeworkapplication.model.Person;
+
+import java.util.List;
+
 // TODO: 2/3/2021 Create adapter to take a List<Person>
-public class PersonAdapter {
+public class PersonAdapter extends RecyclerView.Adapter<PersonAdapter.ViewHolder> {
+
+    private List<Person> people;
+
+    public PersonAdapter(List<Person> people) {
+        this.people = people;
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        private final ItemLayoutBinding binding;
+
+        public ViewHolder(ItemLayoutBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
+        }
+
+        public void setPeople(Person person) {
+
+            binding.tvDisplay.setText(person.getFirstName());
+        }
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        ItemLayoutBinding binding = ItemLayoutBinding.inflate(
+                LayoutInflater.from(parent.getContext()),
+                parent,
+                false
+        );
+        return new ViewHolder(binding);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Person person = people.get(position);
+        holder.setPeople(person);
+    }
+
+    @Override
+    public int getItemCount() {
+        return people.size();
+    }
+
+
+
 }
--- app/src/main/java/com/example/homeworkapplication/view/MainActivity.java
+++ app/src/main/java/com/example/homeworkapplication/view/MainActivity.java
@@ -1,16 +1,47 @@
 package com.example.homeworkapplication.view;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 import com.example.homeworkapplication.R;
+import com.example.homeworkapplication.adapter.PersonAdapter;
+import com.example.homeworkapplication.databinding.ActivityMainBinding;
+import com.example.homeworkapplication.model.Person;
+import com.example.homeworkapplication.viewmodel.MainViewModel;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     // TODO: 2/3/2021 Get data from viewmodel and load into recyclerview
+
+    private MainViewModel viewModel;
+    private ActivityMainBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        viewModel = new ViewModelProvider(this).get(MainViewModel.class);
+
+        viewModel.loadPeopleList();
+
+        viewModel.getPeople().observe(this, new Observer<List<Person>>() {
+            @Override
+            public void onChanged(List<Person> people) {
+                PersonAdapter adapter = new PersonAdapter(people);
+                binding.rvList.setAdapter(adapter);
+            }
+        });
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        binding.rvList.setLayoutManager(linearLayoutManager);
+
     }
 }
